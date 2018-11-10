75f2f8d92864c5d910c9a6a0aec6e9585470ebda
Winston <bjdhjj@gmail.com>
2018-11-10 14:22:05 +0800

    add button

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+if [ -n "$PASSWORD" ];then
+    sed -i "s/herosocks/$PASSWORD/g" /root/config.json
+    echo "You've set a password"
+    echo "The password is: $PASSWORD"
+else
+    echo "You didn't set a password"
+    echo "Use default password: herosocks"
+fi
 sed -i "s/PORT/$PORT/g" /etc/nginx/conf.d/default.conf
 mkdir -p /var/tmp/nginx/client_body
 mkdir /run/nginx
